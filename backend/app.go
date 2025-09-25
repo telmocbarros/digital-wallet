bd1074dba96fe9954341871b85b3c777da7551f4
Telmo Barros <somebodyceo@gmail.com>
2025-09-25 15:59:15 +0200

    setup simple go server

---
+++ backend/app.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Initialise the Gin router
+	r := gin.Default()
+
+	// GET endpoit at "/"
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "Hello, World!"})
+	})
+
+	// Start server on port 8080 (default)
+	fmt.Print("Server started at PORT 8080")
+	r.Run()
+}
