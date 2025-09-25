37d934f24db2f8e08c4a60cf1068fc8598f5579e
Telmo Barros <somebodyceo@gmail.com>
2025-09-25 16:56:56 +0200

    Allow requests from any origin for development purposes

--- backend/app.go
+++ backend/app.go
@@ -11,6 +11,11 @@ func main() {
 	// Initialise the Gin router
 	r := gin.Default()
 
+	// Middleware to handle CORS
+	r.Use(func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Next()
+	})
 	// GET endpoit at "/"
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"message": "Hello, World!"})
