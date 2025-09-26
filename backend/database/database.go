4259541d8032c6fd4d6cb114578b3e6d6139b9f4
Telmo Barros <somebodyceo@gmail.com>
2025-09-26 15:59:09 +0200

    set up simple in-memory database

---
+++ backend/database/database.go
@@ -0,0 +1,12 @@
+package database
+
+type User struct {
+	Id       string `json:"id"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+var Data []User = []User{
+	{Id: "b18b851a-c8c4-4957-b68a-14362a1810c6", Email: "john@example.com", Password: "password123"},
+	{Id: "b5ed9407-681b-4dbb-b2d3-997803e8bbfc", Email: "jane@example.com", Password: "securepass"},
+}
