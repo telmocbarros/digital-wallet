f036069a1b1ff5c4a178303a75e125ebabd404d8
Telmo Barros <somebodyceo@gmail.com>
2025-09-26 17:48:55 +0200

    added fetching user functionality

--- backend/database/database.go
+++ backend/database/database.go
@@ -10,3 +10,12 @@ var Data []User = []User{
 	{Id: "b18b851a-c8c4-4957-b68a-14362a1810c6", Email: "john@example.com", Password: "password123"},
 	{Id: "b5ed9407-681b-4dbb-b2d3-997803e8bbfc", Email: "jane@example.com", Password: "securepass"},
 }
+
+func GetUserById(id string) (User, bool) {
+	for _, user := range Data {
+		if user.Id == id {
+			return user, true
+		}
+	}
+	return User{}, false
+}
